9a5e9956b9e97d2ce23cf07cf5bd2448ffb51a66
Andrew Johnson <ajj@byu.net>
2018-01-13 22:54:51 -0600

    Can delete rows

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,23 @@ function start() {
     console.log('jq srced');
 
     getList();
+
+   $('main').on('click', '.delete', deleteItem);
+}
+
+function deleteItem() {
+    let thisId = $(this).closest('tr').data('id');
+    $.ajax({
+        type: 'DELETE',
+        url: '/list/' + thisId,
+        success: function(response) {
+            console.log(response);
+            getList();
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 }
 
 function getList() {
@@ -21,11 +38,12 @@ function getList() {
 }
 
 function showList(list) {
-    let newTable = $('<table>');
-    let header = $('<tr><th>Status</th><th>Description</th><th>Actions</th></tr>');
+    let newTable = $('<table id="todos">');
+    let header = $('<tr class="todo"><th>Status</th><th>Description</th><th>Actions</th></tr>');
     newTable.append(header);
     for (let i = 0; i < list.length; i++) {
-        let newRow = $(`<tr><td>x</td><td>${list[i].content}</td><td><button>Delete</button></td></tr>`);
+        let newRow = $(`<tr><td>x</td><td>${list[i].content}</td><td><button class="delete">Delete</button></td></tr>`);
+        newRow.data(list[i]);
         newTable.append(newRow);
     }
     $('main').empty();
--- server/routes/main.route.js
+++ server/routes/main.route.js
@@ -14,4 +14,16 @@ router.get('/', (req, res) => {
         });
 });
 
+router.delete('/:id', (req, res) => {
+    const queryText = 'DELETE FROM todos WHERE id = $1';
+    pool.query(queryText, [req.params.id])
+        .then(result => {
+            console.log('result.rows', result.rows);
+            res.send(result.rows);
+        }).catch(err => {
+            console.log('err', err);
+            res.sendStatus(500);
+        });
+});
+
 module.exports = router;
